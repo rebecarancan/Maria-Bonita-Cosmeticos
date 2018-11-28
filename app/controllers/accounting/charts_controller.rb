d745e95b98998db62a18b43c7535ae693e179a68
Rebeca Rancan <rebecarancanrr@gmail.com>
2018-11-28 17:26:19 -0200

    Configurando gráficos

--- app/controllers/accounting/charts_controller.rb
+++ app/controllers/accounting/charts_controller.rb
@@ -2,7 +2,9 @@ module Accounting
   class ChartsController < ApplicationController
 
     def index
-
+      @sales_groups = MasterSales.each do |sale| {
+        sale.income_total(1)
+      }
     end
   end
 end
