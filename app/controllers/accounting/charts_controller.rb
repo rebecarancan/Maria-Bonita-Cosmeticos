186b105cc4e29a71f6acd556aff58d2d59f13dec
Rebeca Rancan <rebecarancanrr@gmail.com>
2018-12-03 17:46:10 -0200

    Ajustando gráficos

--- app/controllers/accounting/charts_controller.rb
+++ app/controllers/accounting/charts_controller.rb
@@ -2,7 +2,7 @@ module Accounting
   class ChartsController < ApplicationController
 
     def index
-      master_sales =  MasterSale.group_by_month_of_year('master_sales.created_at', format: '%b / %y').joins(sales: :income_type).where(sales: { income_types: { name: 'Dinheiro'}}).sum(:value_cents)
+      master_sales =  MasterSale.group(:year).group(:month).joins(sales: :income_type).where(sales: { income_types: { name: 'Dinheiro'}}).sum(:value_cents)
 
       master_sales.each do |key, value|
         master_sales[key] = value.to_f/100.00
