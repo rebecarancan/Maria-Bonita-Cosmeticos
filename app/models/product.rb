7d259ff99261cb82003908fd6aa106a585f52606
Rebeca Rancan <rebecarancanrr@gmail.com>
2018-12-07 14:35:16 -0200

    Ajustes de CSS

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   # Rails Money
   monetize :cost_cents
 
+  # Search
   include PgSearch
   pg_search_scope :search_by_text, against: [:code, :name],
     using: {
