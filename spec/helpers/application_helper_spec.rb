ad437ca511c2c46eb856c6a1fb8a9a9e12557f8d
Rebeca Rancan <rebecarancanrr@gmail.com>
2018-12-27 16:35:15 -0200

    Helper specs: Application and MasterSales

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,24 +4,21 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe '#value_total(obj)' do
     it 'Calculates the total value of sales' do
       master_sale = create(:master_sale_with_sales)
-      expect(value_total(master_sale.sales)).to eq(master_sale.sales.all.sum(&:value))
-    end
-
-    it 'Calculates the total value of notes' do
-      master_note = create(:master_note_with_notes)
-      expect(value_total(master_note.notes)).to eq(master_note.notes.all.sum(&:value))
-    end
-
-    it 'Calculates the total value of orders' do
-      master_order = create(:master_order_with_orders)
-      expect(value_total(master_order.orders)).to eq(master_order.orders.all.sum(&:value))
+      expect(value_total(master_sale.sales)).to eq(Money.new(50000, 'BRL'))
     end
   end
 
   describe '#calc_balance(obj, balance)' do
     it 'Calculates the balance of master_finance' do
       master_finance = create(:master_finance_with_finances)
-      expect().to eq()
+      expect(calc_balance(master_finance.finances, master_finance.initial_balance)).to eq(Money.new(265000, 'BRL'))
+    end
+  end
+
+  describe '#income_calc(obj)' do
+    it 'Calculates the total value of incomes' do
+      master_finance = create(:master_finance_with_finances)
+      expect(income_calc(master_finance.finances)).to eq(Money.new(250000, 'BRL'))
     end
   end
 
--- spec/helpers/master_sales_helper_spec.rb
+++ spec/helpers/master_sales_helper_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe MasterSalesHelper, type: :helper do
   describe '#total_income(obj, type)' do
     it "Calculates the total value of 'Dinheiro'" do
       master_sale = create(:master_sale_with_sales)
-      expect(total_income(master_sale.sales, 'Dinheiro')).to eq(master_sale.sales.select { |sale| sale.income_type.name == 'Dinheiro' }.sum(&:value))
+      expect(total_income(master_sale.sales, 'Dinheiro')).to eq(Money.new(50000, 'BRL'))
     end
   end
 
   describe '#total_days(obj, type)' do
     it "Calculates the total of days for 'Dinheiro'" do
       master_sale = create(:master_sale_with_sales)
-      expect(total_days(master_sale.sales, 'Dinheiro')).to eq(master_sale.sales.select { |sale| sale.income_type.name == 'Dinheiro' }.count)
+      expect(total_days(master_sale.sales, 'Dinheiro')).to eq(10)
     end
   end
 
@@ -20,6 +20,13 @@ RSpec.describe MasterSalesHelper, type: :helper do
     it "Calculates the average for 'Dinheiro'" do
       master_sale = create(:master_sale_with_sales)
       expect(income_average(master_sale.sales, 'Dinheiro')).to eq(master_sale.sales.from_type('Dinheiro').average(:value_cents))
+    end
+  end
+
+  describe '#sale_color(sale)' do
+    it "Determines the css class" do
+      master_sale = create(:master_sale_with_sales)
+      expect(sale_color('Dinheiro')).to eq('class= income-color style=background:#d9b3ff')
     end
   end
 
